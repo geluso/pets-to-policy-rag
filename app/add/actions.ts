f6c03a501305c806baf6dc69f99a2ff2fdebdbd8
Steve Geluso <stevegeluso@gmail.com>
2025-01-31 12:59:50 -0800

    embed docs in add doc

--- app/add/actions.ts
+++ app/add/actions.ts
@@ -1,9 +1,33 @@
 "use server"
 
 import prisma from "@/lib/prisma";
+import { OpenAIEmbeddings } from '@langchain/openai'
+import { NeonPostgres } from "@langchain/community/vectorstores/neon";
+
+const embeddings = new OpenAIEmbeddings({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const vectorStore = await NeonPostgres.initialize(embeddings, {
+  connectionString: process.env.DATABASE_URL as string,
+});
 
 export async function createDocument({url, title, text}: { url: string, title: string, text: string }) {
-  return await prisma.document.create({ data: { url, title, text }})
+  // 1. Create the document as a row in SQL
+  // 2. Obtain the document SQL row ID
+  // 3. Prepare the document for embedding
+  // 4. Create the embedding with the SQL row ID in metadata
+  const doc = await prisma.document.create({ data: { url, title, text }})
+  const docId = doc.id
+
+  const docsToEmbed = [
+    { pageContent: text, metadata: { documentId: docId } },
+  ];
+  const vectorIds = await vectorStore.addDocuments(docsToEmbed);
+  if (vectorIds.length !== 1) {
+    console.warn('CreateDocument error expected 1 item created got', vectorIds.length)
+  }
+  return doc
 }
 
 export async function getAllDocs() {
--- app/embeddings/page.tsx
+++ app/embeddings/page.tsx
@@ -3,7 +3,6 @@ import { NeonPostgres } from "@langchain/community/vectorstores/neon";
 
 const embeddings = new OpenAIEmbeddings({
   apiKey: process.env.OPENAI_API_KEY,
-  // modelName: 'gpt-4-1106-preview'
 })
 
 const vectorStore = await NeonPostgres.initialize(embeddings, {
@@ -11,27 +10,27 @@ const vectorStore = await NeonPostgres.initialize(embeddings, {
 });
 
 export default async function Vectors() {
-  // const documents = [
-  //   { pageContent: "Hello world", metadata: { topic: "greeting" } },
-  //   { pageContent: "Bye bye", metadata: { topic: "greeting" } },
-  //   {
-  //     pageContent: "Mitochondria is the powerhouse of the cell",
-  //     metadata: { topic: "science" },
-  //   },
-  // ];
-  // const idsInserted = await vectorStore.addDocuments(documents);
-  // console.log('ids inserted', idsInserted)
+  const documents = [
+    { pageContent: "Hello world", metadata: { topic: "greeting" } },
+    { pageContent: "Bye bye", metadata: { topic: "greeting" } },
+    {
+      pageContent: "Mitochondria is the powerhouse of the cell",
+      metadata: { topic: "science" },
+    },
+  ];
+  const idsInserted = await vectorStore.addDocuments(documents);
+  console.log('ids inserted', idsInserted)
   
   // You can now query the store for similar documents to the input query
   const resultOne = await vectorStore.similaritySearch("hola", 1);
-  console.log(resultOne);  
 
-  //const query = 'hotdog'
-  //const embeddingData = await embeddings.embedQuery(query)
-  //console.log('create embeddings for', query)
-  //console.log(embeddingData)
+  vectorStore.similaritySearch("hola", 1, { topic: "science" })
+
+  console.log(resultOne);  
+  console.log(resultOne);  
   return <div>
     <h1>Embeddings</h1>
     <p>{resultOne[0].pageContent}</p>
+    <p>{resultOne[0].metadata.topic}</p>
   </div>
 }
