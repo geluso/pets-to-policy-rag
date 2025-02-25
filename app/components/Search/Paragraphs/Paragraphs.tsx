3526455a2d5564dbcedb2bdd3334307b4546cbdb
Steve Geluso <stevegeluso@gmail.com>
2025-02-25 14:19:26 -0800

    remove margin above 'Answer' margin

--- app/components/Search/Paragraphs/Paragraphs.tsx
+++ app/components/Search/Paragraphs/Paragraphs.tsx
@@ -8,7 +8,7 @@ export default function Paragraphs({paragraphs}: Props) {
     console.log('paragraphs', paragraphs)
     return (
         <div className="w-full h-full border-1 flex-col gap-1">
-            {paragraphs.length > 0 && <h3>💡 Answer</h3>}
+            {paragraphs.length > 0 && <h3 className="mt-0">💡 Answer</h3>}
             {paragraphs.map((paragraph, index) => (
                 <span key={`paragraph-${index}`}>
                     {paragraph.map(({isImportant, text}, index) => (
