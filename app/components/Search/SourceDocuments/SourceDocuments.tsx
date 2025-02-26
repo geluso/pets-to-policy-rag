5b70f0db441cf946a2d04d676392574e24540ddd
Steve Geluso <stevegeluso@gmail.com>
2025-02-26 15:40:12 -0800

    full source document text

--- app/components/Search/SourceDocuments/SourceDocuments.tsx
+++ app/components/Search/SourceDocuments/SourceDocuments.tsx
@@ -12,7 +12,7 @@ export default function SourceDocuments({sourceDocuments}: Props) {
         <div key={`source-${index}`} className="p-1 mr-1">
           <h3 className="mt-0">📄 Source Document</h3>
           <div>{sourceDocument.title}</div>
-          <div>{sourceDocument.text.substring(0, 300) + '...'}</div>
+          <div>{sourceDocument.text}</div>
           <div className="mt-2">
             <Link href={sourceDocument.url}>{sourceDocument.url}</Link>
           </div>
