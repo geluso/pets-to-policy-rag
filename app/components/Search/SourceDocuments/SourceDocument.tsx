121defdbe766b5852e2aa7799053d61df3d20d7a
Steve Geluso <stevegeluso@gmail.com>
2025-02-27 14:53:13 -0800

    use new types in _mock

--- app/components/Search/SourceDocuments/SourceDocument.tsx
+++ app/components/Search/SourceDocuments/SourceDocument.tsx
@@ -4,10 +4,12 @@ import Link from "next/link"
 export default function SourceDocument({ sourceDocument }: { sourceDocument: SourceDocumentProps }) {
   return <div className="border-solid border-black p-1 mr-1">
     <h3 className="mt-0">📄 Source Document</h3>
-    <div>{sourceDocument.title}</div>
-    <div>{sourceDocument.text}</div>
-    <div className="mt-2">
-      <Link href={sourceDocument.url}>{sourceDocument.url}</Link>
+    <div>
+      <div>{sourceDocument.title}</div>
+      <div>{sourceDocument.text}</div>
+      <div className="mt-2">
+        <Link href={sourceDocument.url}>{sourceDocument.url}</Link>
+      </div>
     </div>
   </div>
 }
