063017f47a728da66b40a39ffb24f3ee1f927c40
Steve Geluso <stevegeluso@gmail.com>
2025-02-28 16:37:20 -0800

    better errers

--- app/api/indexed-chunks/route.ts
+++ app/api/indexed-chunks/route.ts
@@ -1,14 +1,19 @@
 'use server'
 
 import prisma from "@/lib/prisma"
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(req: NextRequest) {
-  const json = await req.json()
-  const url: string = json.url
-  const chunkText: string = json.chunkText
-  const chunkIndex: number = json.chunkIndex
+  try {
+    const json = await req.json()
+    const url: string = json.url
+    const chunkText: string = json.chunkText
+    const chunkIndex: number = json.chunkIndex
 
-  const indexedChunk = await prisma.indexed_chunk.create({ data: { url, chunk_text: chunkText, chunk_index: chunkIndex }})
-  return indexedChunk
+    const indexedChunk = await prisma.indexed_chunk.create({ data: { url, chunk_text: chunkText, chunk_index: chunkIndex }})
+    return NextResponse.json(indexedChunk, { status: 201 }) // Respond with JSON and HTTP 201 Created
+  } catch (error) {
+    console.error('POST /api/indexed-chunks', error)
+    return NextResponse.json({ error: `${error}` }, { status: 500 })
+  }
 }
--- app/api/summary-readables/route.ts
+++ app/api/summary-readables/route.ts
@@ -1,13 +1,18 @@
 'use server'
 
 import prisma from "@/lib/prisma"
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(req: NextRequest) {
-  const json = await req.json()
-  const url: string = json.url
-  const summaryReadable: string = json.summaryText
+  try {
+    const json = await req.json()
+    const url: string = json.url
+    const summaryReadable: string = json.summaryText
 
-  const newSummaryReadable = await prisma.summary_readable.create({ data: { url, summary_readable: summaryReadable }})
-  return newSummaryReadable
+    const newSummaryReadable = await prisma.summary_readable.create({ data: { url, summary_readable: summaryReadable }})
+    return NextResponse.json(newSummaryReadable, { status: 201 }) // Respond with JSON and HTTP 201 Created
+  } catch (error) {
+    console.error('POST /api/summary-readables', error)
+    return NextResponse.json({ error: `${error}` }, { status: 500 })
+  }
 }
