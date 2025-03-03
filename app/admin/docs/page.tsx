df8d632ab4346fce6c10d401cee12247d2d1ee4b
Steve Geluso <stevegeluso@gmail.com>
2025-03-03 14:22:37 -0800

    create docs prompts admin pages

---
+++ app/admin/docs/page.tsx
@@ -0,0 +1,5 @@
+export default function AdminDocsPage() {
+  return <div>
+    <h1>Admin Docs</h1>
+  </div>
+}
---
+++ app/admin/prompts/page.tsx
@@ -0,0 +1,5 @@
+export default function AdminPromptsPage() {
+  return <div>
+    <h1>Admin Prompts</h1>
+  </div>
+}
